973e07d3ac2582ddb7d787b265653d9bf7648fe2
Leandro FB Lima <leandrofbl35@gmail.com>
2024-02-04 22:14:20 -0300

    Primeiro programa em shell script

---
+++ mod-1/timer.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# Timer - Em quanto tempo deseja desligar o sistema?
+# Autor: Leandro F B Lima
+# Manutenção: Leandro F B Lima
+
+
+# --------------------------DESCRIÇÃO------------------------- #
+
+# Script simples usado para desligar o sistema, de
+# acordo com o tempo em minutos definido pelo 
+# usuário
+
+
+# --------------------------CHANGELOG------------------------- #
+
+# HISTÓRICO DE ATUALIZAÇÕES DO PROJETO
+#   V 1.0 04/02/2024, LeandroF B LIMA:
+#       -Variavel de tempo e comando adicionados
+#   
+
+
+# --------------------------TESTES------------------------- #
+
+# Testando em:
+#   Bash 5.2.15
+
+
+# --------------------------VARIAVEIS------------------------- #
+
+TEMPO=$1
+COMANDO=$(shutdown -h $TEMPO)
+
+
+# --------------------------EXECUÇÃO------------------------- #
+
+echo "$COMANDO"
+echo "O Sistema será desligado em "$TEMPO" minutos"
+
+
